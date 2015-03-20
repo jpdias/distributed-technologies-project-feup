6e74bd29fc609118e644168e1a5173621b5e1d22
Joao Pedro Dias <jpdias@live.com.pt>
2015-03-20 00:40:31 +0000

    WPF Interface working on it

--- Client/ClientApp.cs
+++ Client/ClientApp.cs
@@ -180,26 +180,6 @@ namespace Client
 
         /* Mechanism for instanciating a remote object through its interface, using the config file */
 
-        class RemoteNew
-        {
-            private static Hashtable types = null;
-
-            private static void InitTypeTable()
-            {
-                types = new Hashtable();
-                foreach (WellKnownClientTypeEntry entry in RemotingConfiguration.GetRegisteredWellKnownClientTypes())
-                    types.Add(entry.ObjectType, entry);
-            }
-
-            public static object New(Type type)
-            {
-                if (types == null)
-                    InitTypeTable();
-                WellKnownClientTypeEntry entry = (WellKnownClientTypeEntry)types[type];
-                if (entry == null)
-                    throw new RemotingException("Type not found!");
-                return RemotingServices.Connect(type, entry.ObjectUrl);
-            }
-        }
+       
     }
 }
---
+++ DESClient/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DESClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            App.IDes.Login(Username.ToString(), Password.ToString());
+        }
+
+        private static int count = 0;
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+           
+            if(count==0)
+            {
+                Login.Visibility = Visibility.Hidden;
+                NameLabel.Visibility = Visibility.Visible;
+                Name.Visibility = Visibility.Visible;
+                count++;
+            }
+            else if(count==1)
+            {
+                var user = Username.ToString();
+                var name = Name.ToString();
+                var password = Password.ToString();
+                App.IDes.AddUser(user,name,password);
+                App.IDes.Login(user, password);
+                count=0;
+            }
+        }
+    }
+}
