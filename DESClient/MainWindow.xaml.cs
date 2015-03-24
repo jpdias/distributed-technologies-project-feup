21cf1f4c2b2eee8f5282b7ba1c997ea0c4022e05
Joao Pedro Dias <jpdias@live.com.pt>
2015-03-24 22:59:12 +0000

    Interface update

--- DESClient/MainWindow.xaml.cs
+++ DESClient/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace DESClient
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string _username;
+        private string _password;
         public MainWindow()
         {
             InitializeComponent();
@@ -27,29 +29,113 @@ namespace DESClient
 
         private void Login_Click(object sender, RoutedEventArgs e)
         {
-            App.IDes.Login(Username.ToString(), Password.ToString());
+            var user = Username.Text;
+            var password = Password.Password;
+            if (user != null && password != null)
+            {
+
+                var status = App.IDes.Login(user, password);
+                if (status == "Login successful!")
+                {
+                    _username = user;
+                    _password = password;
+                    register.Visibility = Visibility.Hidden;
+                    login.Visibility = Visibility.Hidden;
+                    main.Visibility = Visibility.Visible;
+                    menu.Header = "Welcome " + user + "!";
+                }
+                else
+                    infobox.Text = status;
+            }
+            else
+            {
+                infobox.Text = "Fields can't be empty";
+            }
         }
 
-        private static int count = 0;
         private void Register_Click(object sender, RoutedEventArgs e)
         {
            
-            if(count==0)
+            var user = UsernameNew.Text;
+            var password = PasswordNew.Password;
+            var passwordR = PasswordNewR.Password;
+            var name = NameNew.Text;
+            if (passwordR != null && name != null && password != null && user != null && user.Length >= 6 && password.Length >= 6 && name.Length >= 6 && passwordR.Length == password.Length)
             {
-                Login.Visibility = Visibility.Hidden;
-                NameLabel.Visibility = Visibility.Visible;
-                Name.Visibility = Visibility.Visible;
-                count++;
+                if (password == passwordR)
+                {
+                    var status = App.IDes.AddUser(name, user, password);
+                    if (status == "User added successfully!")
+                    {
+                        App.IDes.Login(user, password);
+                        _username = user;
+                        _password = password;
+                        show_dashboard();
+                        
+                    }
+                    else
+                        infobox.Text = status;
+                    
+                }
+                else
+                {
+                    infoboxreg.Text = "Passwords don't match.";
+                }
             }
-            else if(count==1)
+            else
             {
-                var user = Username.ToString();
-                var name = Name.ToString();
-                var password = Password.ToString();
-                App.IDes.AddUser(user,name,password);
-                App.IDes.Login(user, password);
-                count=0;
+                infoboxreg.Text = "All fields must have more than 6 characters.";
             }
+
+        }
+
+        private void Registerbox_Click(object sender, RoutedEventArgs e)
+        {
+            login.Visibility = Visibility.Hidden;
+            register.Visibility = Visibility.Visible;
+        }
+
+        private void LoginBack_Click(object sender, RoutedEventArgs e)
+        {
+            register.Visibility = Visibility.Hidden;
+            login.Visibility = Visibility.Visible;
+        }
+
+        private void logout_click(object sender, RoutedEventArgs e)
+        {
+            var status = App.IDes.Logout(_username, _password);
+         
+            if (status.Equals("Logout successful!"))
+            {
+                main.Visibility = Visibility.Hidden;
+                register.Visibility = Visibility.Hidden;
+                login.Visibility = Visibility.Visible;
+               
+            }
+        }
+
+        private void show_dashboard()
+        {
+            register.Visibility = Visibility.Hidden;
+            login.Visibility = Visibility.Hidden;
+            main.Visibility = Visibility.Visible;
+            menu.Header = "Welcome " + _username + "!";
+            //offers;
+        }
+
+        private void Changepw_click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Buy_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Sell_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
